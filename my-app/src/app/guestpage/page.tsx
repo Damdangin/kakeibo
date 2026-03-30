ae52fda8f86cba5111e5db663d917a32fd5a08ce
박재훈 <ttodggs@gmail.com>
2026-03-30 17:59:00 +0900

    mypage作成中

--- my-app/src/app/guestpage/page.tsx
+++ my-app/src/app/guestpage/page.tsx
@@ -178,19 +178,19 @@ export default function GuestKakeiboPage() {
         {/* 【右側】：数値表示 */}
         <div className="p-10 md:w-7/12 bg-white flex flex-col space-y-8">
           <div className="flex justify-end space-x-6 mb-4">
-            <Link
-              href="/login"
-              className="text-sm font-bold text-zinc-500 hover:text-indigo-600"
-            >
-              ログイン
+            <Link href="/login">
+              <button className="text-sm font-bold text-zinc-500 hover:text-indigo-600 transition-colors py-2">
+                ログイン
+              </button>
             </Link>
-            <Link
-              href="/signup"
-              className="bg-indigo-600 text-white px-4 py-2 rounded-xl text-sm font-bold hover:bg-indigo-700 shadow-sm"
-            >
-              新規ID登録
+            <Link href="/signup">
+              <button className="bg-indigo-600 text-white px-4 py-2 rounded-xl text-sm font-bold hover:bg-zinc-200 transition-all shadow-sm">
+                新規ID登録
+              </button>
             </Link>
           </div>
+
+          {/* className="bg-indigo-600 text-white px-4 py-2 rounded-xl text-sm font-bold hover:bg-indigo-700 shadow-sm" */}
 
           <div className="grid grid-cols-1 gap-6">
             <div className="flex justify-between items-center bg-zinc-50 p-6 rounded-3xl border border-zinc-100">
